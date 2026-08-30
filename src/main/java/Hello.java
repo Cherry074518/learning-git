3257d9455b5e0b3c2a9531b75cadd905f1373bd6
Cherry074518 <arumallacharishma@gmail.com>
2026-08-30 22:47:18 +0530

    branch commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
-    public void greet() {
+    public static void greet() {
         System.out.println("This is new method from my branch");
     }
 }
